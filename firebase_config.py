d5f57e83aa8f38192bb423feb7a48c85bef9ad46
hexfoxglove <hexfoxglove@gmail.com>
2025-08-15 17:34:55 +0000

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,24 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Path to your service account key
-cred = credentials.Certificate("/content/drive/MyDrive/dvla_project/firebase_key.json")
+# Use Firebase credentials from Streamlit Secrets
+cred_dict = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"],
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+}
 
 # Initialize Firebase app (only once)
 if not firebase_admin._apps:
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 # Get Firestore client
